970d21f9b1ba0ebe0495b8225aac11fb877ef089
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2017-05-04 11:12:26 +0200

    Initial commit

---
+++ src/ADGTools.Library/Models/Fee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADGTools.Library.Models
+{
+
+    class Fee
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+        public string Status { get; set; }
+        public DateTime? DatePaid { get; set; }
+        public bool IsPaid => DatePaid.HasValue;
+        public bool IsMemberFee => Name.Contains("Medlemsavgift");
+        public int? ForYear
+        {
+            get {
+                for (var year = 2010; year <= 2100; year++) {
+                    if (Name.Contains(year.ToString()) || Description.Contains(year.ToString())) return year;
+                }
+                return null;
+            }
+
+        }
+
+    }
+
+}
---
+++ src/ADGTools.Library/Models/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADGTools.Library.Models
+{
+
+    class Person
+    {
+
+        public string IdrottsID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string EmailAddress { get; set; }
+
+        public List<Fee> Fees;
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public int Age
+        {
+            get
+            {
+                var years = DateTime.Today.Year - BirthDate.Year;
+                return years - (DateTime.Today < BirthDate.AddYears(years) ? 1 : 0);
+            }
+        }
+
+        public DateTime? MemberUntil
+        {
+            get
+            {
+                var lastPaidFee = Fees.Where(e => e.IsMemberFee && e.IsPaid && e.ForYear.HasValue).OrderBy(e => e.ForYear).LastOrDefault();
+                if (lastPaidFee == null) return null;
+                return new DateTime(lastPaidFee.ForYear.Value + 1, 4, 30);
+            }
+        }
+
+        public bool IsPayingMember => MemberUntil.HasValue && MemberUntil.Value >= DateTime.Today;
+
+    }
+
+}
